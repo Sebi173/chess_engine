85a8ab52d5585599896284f09ebe9b50bcbbaf11
Sebastian Heri <sebastian.heri@outlook.com>
2023-11-27 12:16:02 +0100

    making pieces clickable

--- chess/views.py
+++ chess/views.py
@@ -5,8 +5,14 @@ from .magic.calculate_coordinates import calculate_coordinates_for_all_pieces
 from .magic.load_starting_position import load_starting_position
 
 def index(request):
+    if request.method == "POST":
+        listPosition = load_starting_position()
+        dicCoordinates = calculate_coordinates_for_all_pieces(listPosition)
+        print(request.POST.get("style", ""))
+        print(request.POST.get("class", ""))
+        return render(request, "index.html", dicCoordinates)
+    
     listPosition = load_starting_position()
-
     dicCoordinates = calculate_coordinates_for_all_pieces(listPosition)
 
     return render(request, "index.html", dicCoordinates)
